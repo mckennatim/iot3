9bef95f2a7fcd964feb3f21f7c94e9ca78799193
mckennatim <mckenna.tim@gmail.com>
2023-01-09 16:01:42 -0500

    01-blink

--- my/src/main.cpp
+++ my/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // Arduino: turn the LED on (HIGH)
+                                     // D1 Mini: turns the LED *off*
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // Arduino: turn the LED off (LOW)
+                                     // D1 Mini: turns the LED *on*
+  delay(1000);                       // wait for a second
 }
